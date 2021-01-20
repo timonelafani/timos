871e6ab55d37b2ddfa3536fc077302d799f26178
timonelafani <timonelafani@gmail.com>
2021-01-20 18:15:22 +0100

    add theme-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import "../styles/globals.css";
+import { ThemeProvider } from "theme-ui";
+import theme from "../utils/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
